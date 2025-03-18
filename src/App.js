67bfa720059f27adbe26b2e8974fb9708f1f97ae
bsi8616 <bhsingh@hollandamerica.com>
2025-03-18 16:54:32 +0530

    Added Files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Hello React!</h1>
+      {/* <button onClick={() => setCount(count + 1)}>
+        Count: {count}
+      </button> */}
+    </div>
+  );
+}
+
+export default App;
